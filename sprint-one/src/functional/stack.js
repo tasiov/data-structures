e8faebea5fd81c0036b0e91cd9aa573dedb3cbd5
tasiov <tasiovictoria@gmail.com>
2016-01-13 14:54:12 -0800

    Implements stack class

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,29 @@
 var Stack = function() {
   var someInstance = {};
+  var size = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
+  someInstance.show = function() {
+    return storage;
+  };
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    size = Math.max(0, size - 1);
+    var popped = storage[size];
+    delete storage[size];
+    return popped;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
