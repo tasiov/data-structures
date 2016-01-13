3c1b5086079869d7c1e9d63b8fe07cf09a4a0e21
tasiov <tasiovictoria@gmail.com>
2016-01-13 15:59:49 -0800

    Implements pseudoclassical instantiation of stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.stackSize = 0;
+  this.storage = {};
 };
 
 
+Stack.prototype = {
+  constructor: Stack,
+  
+  pop: function() {
+    if (this.stackSize > 0) {
+      this.stackSize--;
+      var popped = this.storage[this.stackSize];
+      delete this.storage[this.stackSize];
+      return popped;
+    }
+  },
+
+  push: function(value) {
+    console.log(this);
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+  },
+
+  size: function() {
+    return this.stackSize;
+  } 
+};
