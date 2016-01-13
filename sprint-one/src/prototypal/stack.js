ca97f3430c729e68eadd70af401bf6e4a0cda6a9
tasiov <tasiovictoria@gmail.com>
2016-01-13 15:54:23 -0800

    Implements the prototypal instantiation of stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj['stackSize'] = 0;
+  obj['storage'] = {};
+  return obj;
 };
 
-var stackMethods = {};
 
+var stackMethods = {
 
+  pop: function() {
+    if (this.stackSize > 0) {
+      this.stackSize--;
+      var popped = this.storage[this.stackSize];
+      delete this.storage[this.stackSize];
+      return popped;
+    }
+  },
+
+  push: function(value) {
+    console.log(this);
+    this.storage[this.stackSize] = value;
+    this.stackSize++;
+  },
+
+  size: function() {
+    return this.stackSize;
+  } 
+};
