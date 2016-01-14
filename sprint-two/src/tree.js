51d5a2f0c059ebb229ca88e38f7147a248dcf76f
tasiov <tasiovictoria@gmail.com>
2016-01-13 20:49:50 -0800

    Implements tree constructor

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
   var newTree = Object.create(treeMethods);
   newTree.value = value;
-
+ 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,17 +11,29 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  if (this.children.length === 0) {
+    return false;
+  }
+  var foundTarget = false;
+  for (var i = 0; i < this.children.length; i++) {
+    foundTarget = foundTarget || this.children[i].contains(target);
+  }
+  return foundTarget;
 };
 
-var Node = function(value) {
-  var node = {};
-  node.children = [];
-  node.value = value || null;
-  return node;
-};
+// var Node = function(value) {
+//   var node = {};
+//   node.children = [];
+//   node.value = value || null;
+//   return node;
+// };
 
 /*
  * Complexity: What is the time complexity of the above functions?
