932733cab2aa9688e6a164c0bc317445219aba8e
tasiov <tasiovictoria@gmail.com>
2016-01-14 15:57:26 -0800

    Implements binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,58 @@
 var BinarySearchTree = function(value) {
+  var newBTree = Object.create(bTreeMethods);
+  newBTree.value = value;
+ 
+  var left = null;
+  var right = null;
+
+  return newBTree;
 };
 
+var bTreeMethods = {};
+
+bTreeMethods.insert = function(value) {
+  if (value > this.value) {
+    if (this.right != null) {
+      this.right.insert(value);
+    } else {
+      this.right = new BinarySearchTree(value);
+    }
+  } else {
+    if (this.left != null) {
+      this.left.insert(value);
+    } else {
+      this.left = new BinarySearchTree(value);
+    }
+  }
+};
+
+bTreeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  } 
+  if (target > this.value && this.right != null) {
+    return this.right.contains(target);
+  } else if (target < this.value && this.left != null) {
+    return this.left.contains(target);
+  } else {
+    return false;
+  }
+};
+
+bTreeMethods.depthFirstLog = function(func) {
+  func(this.value);
+  if (this.left != null) {
+    this.left.depthFirstLog(func);
+  }
+  if (this.right != null) {
+    this.right 
+    .depthFirstLog(func);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ *  insert: O(log(n))
+ *  contains: O(log(n))
+ *  depthFirstLog: O(n)
  */
