ad43a81afda23ba2579454d46772c576b5f44968
tasiov <tasiovictoria@gmail.com>
2016-01-13 16:34:17 -0800

    Implements linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,42 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      this.tail.next = newNode;
+    }
+    this.tail = newNode;
   };
 
   list.removeHead = function() {
+    if (this.head === this.tail && this.head !== null) {
+      var val = this.head.value;
+      this.head = null;
+      this.tail = null;
+      return val;
+    } else if (this.head !== null) {
+      var temp = this.head;
+      this.head = this.head.next;
+      temp.next = null;
+      return temp.value;      
+    }
+    return null;
   };
 
   list.contains = function(target) {
+    var pointer = this.head; 
+    while (pointer !== null) {
+      if (pointer.value === target) {
+        return true;
+      } else {
+        pointer = pointer.next;
+      }
+    }
+    return false;
   };
-
+  
   return list;
 };
 
