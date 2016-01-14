9982a484661e1a681236c85b368c7a580aadb644
tasiov <tasiovictoria@gmail.com>
2016-01-13 21:02:19 -0800

    Implements first draft of graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,11 +3,20 @@
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
+  var newGraph = Object.create(Graph.prototype);
+  newGraph.value = null;
+ 
+  // your code here
+  newGraph.edges = [];
+
+  return newGraph;
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  var graphNode = Graph();
+  graphNode.value = node;
 };
 
 // ------------------------
