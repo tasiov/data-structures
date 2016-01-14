1bf9689082aa10fe44f06291af0587c371c19fa5
tasiov <tasiovictoria@gmail.com>
2016-01-13 16:03:04 -0800

    Implements pseudoclassical instantiation of queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.front = 0;
+  this.back = 0;
+  this.queueSize = 0;
+  this.storage = {};
 };
+
+Queue.prototype = {
+  constructor: Queue,
+
+  enqueue: function(value) {
+    this.storage[this.back] = value;
+    this.back++;
+    this.queueSize++;
+  },
+
+  dequeue: function() {
+    var dequeued = this.storage[this.front];
+    delete this.storage[this.front];
+    this.front++;
+    this.queueSize = Math.max(0, this.queueSize - 1);
+    return dequeued;
+  },
+
+  size: function() {
+    return this.queueSize;
+  }
+};
+
 
 
